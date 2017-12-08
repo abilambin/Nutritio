fb35e5f2e6b5cbbf5cf5a395218948425b1efb82
Lambin Abigail <abigail.lambin@etudiant.univ-lille1.fr>
2017-12-08 17:48:52 +0100

    ingredient et main

--- app/src/main/java/com/example/abilambin/nutritio/bdd/model/Ingredient.java
+++ app/src/main/java/com/example/abilambin/nutritio/bdd/model/Ingredient.java
@@ -6,7 +6,28 @@ import java.io.Serializable;
  * Created by serial on 30/11/2017.
  */
 
-class Ingredient implements Serializable{
+public class Ingredient implements Serializable{
+
+    public Ingredient(String name, String brand) {
+        this.name = name;
+        this.brand = brand;
+    }
+
+    public Ingredient(String name, String brand, int energy, float proteins, float carbohydrates, float sugar, float fat, float saturatedFat, float fibres, float salt, float sodium) {
+
+        this.name = name;
+        this.brand = brand;
+        this.energy = energy;
+        this.proteins = proteins;
+        this.carbohydrates = carbohydrates;
+        this.sugar = sugar;
+        this.fat = fat;
+        this.saturatedFat = saturatedFat;
+        this.fibres = fibres;
+        this.salt = salt;
+        this.sodium = sodium;
+
+    }
 
     private static long serialVersionUID = 2L;
 
@@ -14,17 +35,118 @@ class Ingredient implements Serializable{
 
     private String name;
 
-    private int proteines;
-
-    private int glucides;
-
-    private int lipides;
-
-    private int fibres;
-
-    private int sel;
+    private String brand;
 
 
+    private int energy;
+
+    private float proteins;
+
+    private float carbohydrates;
+    private float sugar;
+
+    private float fat;
+    private float saturatedFat;
+
+    private float fibres;
+
+    private float salt;
+    private float sodium;
 
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public static void setSerialVersionUID(long serialVersionUID) {
+        Ingredient.serialVersionUID = serialVersionUID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public float getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(float proteins) {
+        this.proteins = proteins;
+    }
+
+    public float getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(float carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public float getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(float sugar) {
+        this.sugar = sugar;
+    }
+
+    public float getFat() {
+        return fat;
+    }
+
+    public void setFat(float fat) {
+        this.fat = fat;
+    }
+
+    public float getSaturatedFat() {
+        return saturatedFat;
+    }
+
+    public void setSaturatedFat(float saturatedFat) {
+        this.saturatedFat = saturatedFat;
+    }
+
+    public float getFibres() {
+        return fibres;
+    }
+
+    public void setFibres(float fibres) {
+        this.fibres = fibres;
+    }
+
+    public float getSalt() {
+        return salt;
+    }
+
+    public void setSalt(float salt) {
+        this.salt = salt;
+    }
+
+    public float getSodium() {
+        return sodium;
+    }
+
+    public void setSodium(float sodium) {
+        this.sodium = sodium;
+    }
 }
