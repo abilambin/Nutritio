f9238958c8aa23338acc682ae100c0bbd856b8d7
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2018-02-15 22:08:37 +0100

    Start post method

--- app/src/main/java/com/example/abilambin/nutritio/restApi/RestCaller.java
+++ app/src/main/java/com/example/abilambin/nutritio/restApi/RestCaller.java
@@ -1,5 +1,7 @@
 package com.example.abilambin.nutritio.restApi;
 
+import android.provider.MediaStore;
+
 import com.example.abilambin.nutritio.bdd.model.Ingredient;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -9,8 +11,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -18,6 +22,7 @@ import okhttp3.Response;
  */
 
 public class RestCaller <T> implements RestCallerInterface<T>{
+    private final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private String path;
     private TypeToken<List<T>> typeToken;
 
@@ -41,19 +46,33 @@ public class RestCaller <T> implements RestCallerInterface<T>{
 
             String bodyString = response.body().string();
 
-            System.out.println(bodyString);
-
             return new Gson().fromJson(bodyString, this.typeToken.getType());
         } catch (IOException e) {
             e.printStackTrace();
             return null; // TODO
         }
-
     }
 
     @Override
     public T create(T item) {
-        return null;
+        Response response = null;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(this.path)
+                .addHeader("Authorization", RestCallerConstant.AUTH_TOKEN)
+                .post(RequestBody.create(JSON, new Gson().toJson(item)))
+                .build();
+
+        try {
+            response = client.newCall(request).execute();
+
+            String bodyString = response.body().string();
+
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null; // TODO
+        }
     }
 
     @Override
@@ -71,8 +90,4 @@ public class RestCaller <T> implements RestCallerInterface<T>{
         return null;
     }
 
-    public static <T> List<T> stringToArray(String s, Class<T[]> clazz) {
-        T[] arr = new Gson().fromJson(s, clazz);
-        return Arrays.asList(arr); //or return Arrays.asList(new Gson().fromJson(s, clazz)); for a one-liner
-    }
 }
