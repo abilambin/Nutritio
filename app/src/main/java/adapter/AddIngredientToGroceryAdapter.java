5530f1a2f67fbaf1e164119f07e00941f89c2b69
Elymak <camillebreuzon@orange.fr>
2018-03-08 16:54:02 +0100

    Filtre générique

--- app/src/main/java/adapter/AddIngredientToGroceryAdapter.java
+++ app/src/main/java/adapter/AddIngredientToGroceryAdapter.java
@@ -1,13 +1,9 @@
 package adapter;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,37 +11,24 @@ import android.widget.Toast;
 import com.example.abilambin.nutritio.R;
 import com.example.abilambin.nutritio.bdd.model.Ingredient;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by serial on 23/02/2018.
  */
 
-public class AddIngredientToGroceryAdapter extends BaseAdapter implements Filterable {
-    private List<Ingredient> originalsIngredients; //original values
-    private List<Ingredient> filteredIngredients; //displayed values
-    private LayoutInflater inflater;
+public class AddIngredientToGroceryAdapter extends AbstractListAdapter<Ingredient> {
 
-    public AddIngredientToGroceryAdapter(Context context, List<Ingredient> ingredients){
-        originalsIngredients = ingredients;
-        filteredIngredients = ingredients;
-        inflater = LayoutInflater.from(context);
+    public AddIngredientToGroceryAdapter(Context context, List<Ingredient> items){
+        super(context, items);
     }
 
     @Override
-    public int getCount() {
-        return filteredIngredients.size();
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return i;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return i;
+    public boolean filterCondition(Ingredient element, CharSequence constraint) {
+        String name = element.getName();
+        String brand = element.getBrand();
+        brand = brand == null ? "" : brand;
+        return name.toLowerCase().contains(constraint.toString()) || brand.toLowerCase().contains(constraint.toString());
     }
 
     private class ViewHolder {
@@ -61,7 +44,7 @@ public class AddIngredientToGroceryAdapter extends BaseAdapter implements Filter
         if (convertView == null) {
 
             holder = new ViewHolder();
-            convertView = inflater.inflate(R.layout.list_element_add_ingredient, null);
+            convertView = getInflater().inflate(R.layout.list_element_add_ingredient, null);
             holder.infoContainer = convertView.findViewById(R.id.ingredientInfoContainer);
             holder.buttonContainer = convertView.findViewById(R.id.ingredientButtonContainer);
 
@@ -75,57 +58,19 @@ public class AddIngredientToGroceryAdapter extends BaseAdapter implements Filter
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
-        holder.name.setText(filteredIngredients.get(position).getName());
-        holder.category.setText(filteredIngredients.get(position).getBrand());
+        holder.name.setText(getFilteredElements().get(position).getName());
+        holder.category.setText(getFilteredElements().get(position).getBrand());
 
         holder.minus.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
 
-                Toast.makeText(v.getContext(), filteredIngredients.get(position).getName(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(v.getContext(), getFilteredElements().get(position).getName(), Toast.LENGTH_SHORT).show();
             }
         });
 
         return convertView;
     }
 
-    @Override
-    public Filter getFilter() {
-        return new Filter() {
-            @Override
-            protected FilterResults performFiltering(CharSequence constraint) {
-                FilterResults results = new FilterResults();
-                List<Ingredient> filteredArrList = new ArrayList<>();
-                if(originalsIngredients == null){
-                    originalsIngredients = new ArrayList<>(filteredIngredients);
-                }
-                if (constraint == null || constraint.length() == 0) {
 
-                    // set the Original result to return
-                    results.count = originalsIngredients.size();
-                    results.values = originalsIngredients;
-                } else {
-                    constraint = constraint.toString().toLowerCase();
-                    for (int i = 0; i < originalsIngredients.size(); i++) {
-                        String name = originalsIngredients.get(i).getName();
-                        String brand = originalsIngredients.get(i).getBrand();
-                        brand = brand == null ? "" : brand;
-                        if (name.toLowerCase().contains(constraint.toString()) || brand.toLowerCase().contains(constraint.toString())) {
-                            filteredArrList.add(originalsIngredients.get(i));
-                        }
-                    }
-                    // set the Filtered result to return
-                    results.count = filteredArrList.size();
-                    results.values = filteredArrList;
-                }
-                return results;
-            }
-
-            @Override
-            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
-                filteredIngredients = (List<Ingredient>) filterResults.values;
-                notifyDataSetChanged();
-            }
-        };
-    }
 }
