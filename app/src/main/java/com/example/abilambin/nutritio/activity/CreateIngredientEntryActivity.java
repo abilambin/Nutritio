8911a60e8f4c4090a788ed412aa678294b36469d
Elymak <camillebreuzon@orange.fr>
2018-03-09 12:39:16 +0100

    Ajout d'ingrédients dans les listes de courses et dans le stock v1.1

--- app/src/main/java/com/example/abilambin/nutritio/activity/CreateIngredientEntryActivity.java
+++ app/src/main/java/com/example/abilambin/nutritio/activity/CreateIngredientEntryActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.Html;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,15 +14,9 @@ import android.widget.TextView;
 import com.example.abilambin.nutritio.R;
 import com.example.abilambin.nutritio.bdd.model.Ingredient;
 import com.example.abilambin.nutritio.bdd.model.IngredientEntry;
-import com.example.abilambin.nutritio.bdd.model.ingredientList.Grocerie;
-import com.example.abilambin.nutritio.exception.CannotAuthenticateUserException;
-import com.example.abilambin.nutritio.exception.WebServiceCallException;
-import com.example.abilambin.nutritio.bdd.model.IngredientEntry;
 import com.example.abilambin.nutritio.exception.CannotAuthenticateUserException;
 import com.example.abilambin.nutritio.exception.WebServiceCallException;
 import com.example.abilambin.nutritio.fragment.IntakesFragment;
-import com.example.abilambin.nutritio.restApi.specific.GrocerieRestCaller;
-import com.example.abilambin.nutritio.restApi.specific.IngredientEntryRestCaller;
 import com.example.abilambin.nutritio.restApi.GenericRestCaller;
 import com.example.abilambin.nutritio.restApi.specific.IngredientEntryRestCaller;
 
@@ -61,11 +54,6 @@ public abstract class CreateIngredientEntryActivity<T> extends AppCompatActivity
     Button submit;
 
     GenericRestCaller<T> tGenericRestCaller;
-
-    private IngredientEntryRestCaller ingredientEntryRestCaller = new IngredientEntryRestCaller();
-
-    private GrocerieRestCaller groceriesRestCaller = new GrocerieRestCaller();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -120,45 +108,12 @@ public abstract class CreateIngredientEntryActivity<T> extends AppCompatActivity
 
             @Override
             public void onClick(View v) {
-                //Créer IngredientEntry + l'ajouter aux courses
-                IngredientEntry entry = new IngredientEntry();
-                entry.setIngredient(ingredient);
-                entry.setAmount(Integer.parseInt(quantity.getText().toString()));
-
-                try {
-                    ingredientEntryRestCaller.create(entry);
-                    //TODO : changer '1' par l'id de l'utilisateur
-                    Grocerie grocerie = (Grocerie) groceriesRestCaller.get(1);
-                    grocerie.add(entry);
-                    groceriesRestCaller.update(grocerie);
-
-                    Grocerie g = (Grocerie) groceriesRestCaller.get(1);
-                    int i = 1;
-                    for (IngredientEntry e : g.getIngredientEntries()) {
-                        Log.d("INGREDIENTS :" + i + " : ",e.getIngredient().getName());
-
-                        i++;
-                    }
-
-
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (WebServiceCallException e) {
-                    e.printStackTrace();
-                } catch (CannotAuthenticateUserException e) {
-                    e.printStackTrace();
-                }
-
-
                 T type = getType();
                 IngredientEntry newIngredientEntry = createEntry(ingredient, type);
                 if(type != null){
                     updateType(newIngredientEntry, type);
                 }
             }
-
         });
 
     }
--- app/src/main/java/com/example/abilambin/nutritio/fragment/MealIngredientListFragment.java
+++ app/src/main/java/com/example/abilambin/nutritio/fragment/MealIngredientListFragment.java
@@ -1,21 +1,19 @@
 package com.example.abilambin.nutritio.fragment;
 
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.TextView;
-
 import com.example.abilambin.nutritio.R;
 import com.example.abilambin.nutritio.bdd.model.ingredientList.Grocerie;
-import com.example.abilambin.nutritio.restApi.specific.GrocerieRestCaller;
 import com.example.abilambin.nutritio.restApi.specific.IngredientRestCaller;
-
-import butterknife.BindView;
 
 public class MealIngredientListFragment extends IngredientListFragment<Grocerie> {
 
     @Override
     protected int getListLayout() {
         return R.layout.fragment_meal_ingredient_list;
+    }
+
+    @Override
+    public Class getAddIngredientActivity() {
+        return null;
     }
 
     public MealIngredientListFragment(){
